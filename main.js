064d0321eb484c96f441e0ab351e91c1bc53990d
katteryn villanueva <katterynflavia.hv@gmail.com>
2024-09-04 18:02:11 -0400

    aprendiendo las: variables, constantes, mostrar consola, concatenacion, seleccionar elementos del dom y alerta

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-alert("Hola soy Katteryn aprendiendo JavaScript");
+// alert("Hola soy Katteryn aprendiendo JavaScript");
+
+//Variables
+let nombre = "Katteryn";
+nombre = "Rick";
+
+//Constantes
+const apellido = "Villanueva";
+const altura = 154;
+
+//Mostrar por consola
+console.log(nombre);
+console.log(altura);
+
+//Concatenacion
+let concatenacion = nombre + " " + apellido;
+
+//Seleccionar elementos del dom(de la pagina)
+let datos = document.querySelector("#datos");
+datos.innerHTML = `
+        <hr>
+        <h1> Soy la caja de datos</h1>
+        <h2>Mi nombre es: ${concatenacion}</h2>
+        <h3>Mi altura es:${altura} cm</h3>
+`;
+
+//condiciones
